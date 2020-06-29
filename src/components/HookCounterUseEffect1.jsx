18530438d81bf823d40d8209cedc2bdfed78c01d
Jasoliya Manoj <48120921+Manojj999@users.noreply.github.com>
2020-06-29 11:09:35 +0530

    Conditionaly use useEffect

--- src/components/HookCounterUseEffect1.jsx
+++ src/components/HookCounterUseEffect1.jsx
@@ -5,11 +5,15 @@ import React,{useEffect,useState} from 'react'
 //it replace componentDidMount and componentDidUpdate
 function HookCounterUseEffect1() {
     const [count , setCount] = useState(0);
+    const [name , setName] = useState('');
+    
     useEffect(() => {
+        console.log('updating doc title')
         document.title =`you have clicked ${count} Times`
-    })
+    },[count])//[] specify state or props that when to update useEffect | other word useEffect here depend on count value
     return (
         <div>
+            <input type="text" value={name} onChange={e => setName(e.target.value)}/>
             <button onClick={() => setCount(count+1)}> count {count} times</button>
         </div>
     )
